03c481fd8c6c806da6cd211de0cb5e54b6705e46
D.N,Chen <dnchenbeta@gmail.com>
2017-12-28 23:33:12 +0800

    Update weather_full.py

--- weather_full.py
+++ weather_full.py
@@ -17,18 +17,18 @@ import re
 import pandas as pd
 from pandas import Series
 
-def get_weather_bydate(date):
-    url = 'https://www.wunderground.com/history/airport/ZSPD/'+ date +'/DailyHistory.html'
+def get_weather_bydate(date): # 实现每日天气数据的获取
+    url = 'https://www.wunderground.com/history/airport/ZSPD/'+ date +'/DailyHistory.html' # url与日期相关
     page = urllib.request.urlopen(url)
     soup = BeautifulSoup(page, 'lxml')
     
-    indexs = soup.find_all(attrs = {'class':'indent'})
+    indexs = soup.find_all(attrs = {'class':'indent'}) # 特征是class = 'indent'，实际上获取了各种天气数据的名称。
     l1 = []
     for index in indexs:
         ind = index.string.strip()
         l1.append(ind)
     
-    raw = soup.select('tr')
+    raw = soup.select('tr') # 在<tr>框中进行选取，直接用正则比较方便，代码更统一。
     l2 = []
     for each in raw:
         straw = str(each)
@@ -48,12 +48,17 @@ def get_weather_bydate(date):
     df.name = str(date)
     return(df)
     
-def get_date(yr):
+def get_date(yr): # 自己造了一个日期轮子...
     l1 = []
-    for m in range(1, 13): # 获得天气数据的已有实现方案
+    for m in range(1, 13): 
         for d in range(1, 32):
-            if (m == 2 and d > 28):
-                break
+            if m == 2:
+                if yr % 4 == 0:
+                    if d > 29:
+                        break
+                else:
+                    if d > 28:
+                        break
             elif (m in [4,6,9,11] and d > 30):
                 break
             
@@ -73,7 +78,7 @@ def get_date(yr):
             l1.append(timestamp)
     return(l1)
     
-def get_weather(yr):
+def get_weather(yr): # 这里用到的是dataframe的拼接，pd.concat，但是感觉有点复杂了
     l0 = get_date(yr)
     for each in l0:
         temp = get_weather_bydate(each)
